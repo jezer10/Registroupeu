599fb7fd6a9f4c01e052b4a59a725beace86f921
jezer10 <jezerrazuri@upeu.edu.pe>
2020-06-16 11:26:33 -0500

    first commit

---
+++ src/pe/edu/upeu/registro/test/Test.java
@@ -0,0 +1,36 @@
+package pe.edu.upeu.registro.test;
+
+import com.google.gson.Gson;
+
+import pe.edu.upeu.registro.dao.AlumnoDao;
+import pe.edu.upeu.registro.dao.EscuelaDao;
+import pe.edu.upeu.registro.daoImp.AlumnoDaoImp;
+import pe.edu.upeu.registro.daoImp.EscuelaDaoImp;
+import pe.edu.upeu.registro.util.Conexion;
+
+
+public class Test {
+	private static AlumnoDao pd = new AlumnoDaoImp();
+	private static EscuelaDao esc = new EscuelaDaoImp();
+	private static Gson g = new Gson();
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//conex();
+			//listarP();
+			listarC();
+		}
+	  static void listarP() {
+		  System.out.println(g.toJson(pd.readAll()));
+	  }
+	  static void listarC() {
+		  System.out.println(g.toJson(esc.readAll()));
+	  }
+	  static void conex() {
+		  if (Conexion.getConexion()!=null) {
+			System.out.println("Conectado");
+		} else {
+			System.out.println("Desconectado");
+
+		}
+	  }
+}
